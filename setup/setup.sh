02a11355a595dd8cedbb0133dc3ef059f57914fd
martiera <ieris@inbox.lv>
2018-08-30 15:09:29 +0300

    Install gateway and related software

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,100 @@
+#########################################################################
+#  Gateway setup script for RaspberryPi
+#########################################################################
 #!/bin/bash
-mkdir ../logs
-sudo cp ./logrotate /etc/logrotate.d/gateway
-sudo cp ./gateway.service /etc/systemd/system/gateway.service
+
+RED='\033[1;31m'
+GRN='\033[1;32m'
+YLW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+GATEWAY_DIR='/home/pi/gateway/'
+
+echo -e "${GRN}#########################################################################${NC}"
+echo -e "${GRN}#                    OpenMiniHub Gateway Setup                          #${NC}"
+echo -e "${GRN}#########################################################################${NC}"
+echo -e "${YLW}Note: script can take long on older Pis${NC}"
+echo -e "${YLW}Note: setup requires your input at certain steps${NC}"
+
+echo -e "${CYAN}************* STEP: Running apt-get update *************${NC}"
+sudo apt-get update -m
+echo -e "${CYAN}************* STEP: Upgrading distribution *************${NC}"
+sudo apt-get upgrade
+echo -e "${CYAN}************* STEP: Running dist-upgrade *************${NC}"
+sudo apt-get dist-upgrade
+sudo apt-get clean
+
+echo -e "${CYAN}************* STEP: Installing git, minicon *************${NC}"
+sudo apt-get -y install git minicom
+
+echo -e "${CYAN}************* STEP: Installing Mosquitto MQTT *************${NC}"
+sudo apt-get -y install mosquitto mosquitto-clients
+
+#install NodeJS
+echo -e "${CYAN}************* STEP: Installing NodeJS *************${NC}"
+sudo wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash
+
+echo -e "${CYAN}************* STEP: Setup Gateway app & dependencies *************${NC}"
+sudo mkdir -p $GATEWAY_DIR    #main dir where gateway app lives
+cd $GATEWAY_DIR || exit
+git init
+git remote add origin https://github.com/openminihub/gateway.git
+git pull origin master
+sudo npm install --unsafe-perm --build-from-source
+sudo npm cache verify    #clear any caches/incomplete installs
+sudo mkdir $GATEWAY_DIR/logs -p
+
+#create db and empty placeholders so chown pi will override root permissions
+sudo mkdir $GATEWAY_DIR/data -p
+touch $GATEWAY_DIR/data/action.db
+touch $GATEWAY_DIR/data/building.db
+touch $GATEWAY_DIR/data/contact.db
+touch $GATEWAY_DIR/data/contactmessage.db
+touch $GATEWAY_DIR/data/device.db
+touch $GATEWAY_DIR/data/devicetype.db
+touch $GATEWAY_DIR/data/message.db
+touch $GATEWAY_DIR/data/node.db
+touch $GATEWAY_DIR/data/user.db
+
+#create self signed certificate
+#WARNING: must do this *AFTER* the gateway app was git-cloned
+echo -e "${CYAN}************* STEP: Create self signed HTTPS certificate (5 year) *************${NC}"
+sudo mkdir $GATEWAY_DIR/data/secure -p
+sudo openssl req -new -x509 -nodes -days 1825 -newkey rsa:2048 -out $GATEWAY_DIR/ssl/server.crt -keyout $GATEWAY_DIR/ssl/server.key -subj "/C=LV/L=Marupe/O=OpenMiniHub/OU=IoT/CN=openminihub.com"
+sudo chown -R pi:pi $GATEWAY_DIR
+
+echo -e "${CYAN}************* STEP: Disable GPIO serial console *************${NC}"
+sudo raspi-config nonint do_serial 1
+
+echo -e "${CYAN}************* STEP: Configuring logrotate *************${NC}"
+sudo echo "#this is used by logrotate and should be placed in /etc/logrotate.d/
+#rotate the gateway logs and keep a limit of how many are archived
+#note: archives are rotated in $GATEWAY_DIR/logs so that dir must exist prior to rotation
+$GATEWAY_DIR/logs/*.log {
+        size 20M
+        missingok
+        rotate 20
+        dateext
+        dateformat -%Y-%m-%d
+        compress
+        notifempty
+        nocreate
+        copytruncate
+}" | sudo tee /etc/logrotate.d/gateway
+
+echo -e "${CYAN}************* STEP: Setup Gateway service ... *************${NC}"
+sudo cp $GATEWAY_DIR/setup/gateway.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start gateway.service
 sudo systemctl enable gateway.service
+sudo systemctl start gateway.service
+
+echo -e "${RED}! Important : ${YLW}If not done already - configure your Pi core settings (timezone, expand SD etc) by running ${GRN}raspi-config${NC}"
+echo -e "${CYAN}************* STEP: Run raspi-config *************${NC}"
+if (whiptail --title "Run raspi-config ?" --yesno "Would you like to run raspi-config?" 12 78) then
+  sudo raspi-config
+fi
+
+echo -e "${CYAN}************* ALL DONE! *************${NC}"
+cd ~/
+exit 0
