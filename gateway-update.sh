44f1adaf7699448ee2234dec04d6314620dfd4d2
martiera <martins.ierags@gmail.com>
2018-11-21 18:06:10 +0200

    GW update improvement

--- gateway-update.sh
+++ gateway-update.sh
@@ -4,6 +4,7 @@
 
 dir_to_update=$PWD
 gateway_log=~/gateway/logs/gateway.sys.log
+avrdude_log=~/gateway/logs/avrdude.log
 FW="${dir_to_update}/firmware/gateway.ino.hex"
 
 updateRepo() {
@@ -32,19 +33,24 @@ if [ -f "${dir_to_update}/.updatenow" ] ; then
     echo "Updating ${dir_to_update}"
     updateRepo $dir_to_update >> $gateway_log
     mv ${dir_to_update}/.updatenow ${dir_to_update}/.updatedone
-    echo "Gateway update done" >> ${dir_to_update}/.updatedone
     echo "OpenMiniHub gateway has been updated" >> $gateway_log
     echo "Stopping gateway.service" >> $gateway_log
     sudo systemctl kill gateway.service
+    sudo systemctl stop gateway.service
+    gwupdate="done"
     if [ -f "$FW" ] ; then
       FW_NEW=`md5sum $FW | awk '{ print $1 }'`
       if [ "$FW_NEW" != "$FW_OLD" ] ; then
         echo "Updating GW NODE..." >> $gateway_log
-        ./utility/avrdude -v -c arduino -p atmega328p -P /dev/serial0 -b 115200 -U flash:w:$FW > $gateway_log
-      else
-        echo "No GW NODE update this time" > $gateway_log
+        ./utility/avrdude -v -c arduino -p atmega328p -P /dev/serial0 -b 115200 -U flash:w:$FW -l $avrdude_log
+	if [ `grep 'Reading\|Writing' ${avrdude_log} | grep '100%' | wc -l` -ne 3 ] ; then 
+          gwupdate="failed"
+	fi
+        cat $avrdude_log >> $gateway_log
+	rm $avrdude_log
       fi
     fi
+    echo "Gateway update ${gwupdate}" >> ${dir_to_update}/.updatedone
     echo "Starting gateway.service" >> $gateway_log
     sudo systemctl start gateway.service
 else
