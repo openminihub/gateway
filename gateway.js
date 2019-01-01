7c8cef1ca29fb89bee18dcba8cd0aea2f55032af
ieris <ieris@inbox.lv>
2019-01-01 21:35:17 +0200

    Fix ESPurna device detection

--- gateway.js
+++ gateway.js
@@ -421,6 +421,10 @@ function handleOutTopic(rxmessage, nodetype) {
             var _deviceType = 3
             var _msgType = 2
             break
+          case default:
+            var _deviceType = 23
+            var _msgType = 48
+            break
         }
         // MessageDB.update({ $and: [{"nodeid" : msg[1]}, {"deviceid": parseInt(msg[3])}, {"msgtype": parseInt(msg[4])}] }, { $set: { "msgvalue": msg[4], "updated": Math.floor(Date.now()/1000), "rssi": messageRSSI } }, { returnUpdatedDocs : true , multi : false }, function (err, wasAffected, affectedDocument ) {
         // MessageDB.update({ $and: [{ "nodeid" : msg[1], "deviceid": parseInt(msg[3]), "msgtype": _msgType }] }, { "nodeid" : msg[1], "deviceid": parseInt(msg[3]), "devicetype": _deviceType, "msgtype": _msgType, "msgvalue": msg[4], "updated": Math.floor(Date.now()/1000), "rssi": 0 }, { upsert: true }, function (err, wasAffected, affectedDocument, upsert) {
