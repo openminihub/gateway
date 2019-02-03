4e3b40c80d2623f5ee6c725d7e4b261a4cd707dc
martiera <ieris@inbox.lv>
2019-02-03 21:17:24 +0200

    ESPurna: relay & rgb

--- devices/espurna.js
+++ devices/espurna.js
@@ -5,56 +5,61 @@ exports.processMqttData = function (topic, message) {
     var _msg = topic
     message = message.toString()
 
+    var _node = new Object()
+    var _message = new Object()
+    _message.node_id = _msg[1]
+    _message.device_id = _msg[3]
+    _message.value = message
+    _message.rssi = 0
+
     switch (_msg[2]) {
+        case 'rgb':
+            _message.messagetype_id = 40 // 40 = V_RGB
+            break
         case 'relay':
+            _message.messagetype_id = 2 // 2 = V_STATUS
             break
         case 'rfin':
-            var _message = new Object()
-            _message.node_id = _msg[1]
             _message.device_id = message.substring(12)
             _message.messagetype_id = 16 // 16 = V_TRIPPED
             _message.value = '1'
-            _message.rssi = 0
-            debug('Update Messages.db: %s %o', _message.node_id, _message)
-            db.Messages.update({ value: _message.value }, { where: { node_id: _message.node_id, device_id: _message.device_id, messagetype_id: _message.messagetype_id } })
-                .then(rowsUpdated => _doInsertOnNewMessage(rowsUpdated, _message, message))
-                .catch((err) => {
-                    console.log('%s', err)
-                })
             break
-        default:
-            var _node = new Object()
-            switch (_msg[2]) {
-                case 'app':
-                    _node.type = message
-                    break
-                case 'version':
-                    _node.version = message
-                    break
-                case 'board':
-                    _node.board = message
-                    break
-                case 'host':
-                    _node.name = message
-                    break
-                case 'rssi':
-                    // _node.rssi = message
-                    break
-                case 'vcc':
-                    _node.battery = message
-                    break
-            }
-            debug('db.Nodes update: %o', _node)
-            if (!_isEmptyObject(_node)) {
-                _node.id = _msg[1]
-                db.Nodes.upsert(_node)
-                    .then(function () {
-                    })
-                    .catch((err) => {
-                        console.log('%s', err)
-                    })
-            }
+        case 'app':
+            _node.type = message
             break
+        case 'version':
+            _node.version = message
+            break
+        case 'board':
+            _node.board = message
+            break
+        case 'host':
+            _node.name = message
+            break
+        case 'rssi':
+            // _node.rssi = message
+            break
+        case 'vcc':
+            _node.battery = message
+            break
+    }
+    if (!_isEmptyObject(_node)) {
+        debug('db.Nodes update: %o', _node)
+        _node.id = _msg[1]
+        db.Nodes.upsert(_node)
+            .then(function () {
+            })
+            .catch((err) => {
+                console.log('%s', err)
+            })
+    }
+    if (!_isEmptyObject(_message)) {
+        debug('db.Messagess: %o', _message)
+        db.Messages.update({ value: _message.value }, { where: { node_id: _message.node_id, device_id: _message.device_id, messagetype_id: _message.messagetype_id } })
+            .then(rowsUpdated => _doInsertOnNewMessage(rowsUpdated, _message, message))
+            .catch((err) => {
+                console.log('%s', err)
+            })
     }
 }
 
@@ -62,9 +67,19 @@ function _doInsertOnNewMessage(rowsUpdated, values, message) {
     if (rowsUpdated == 0) {
         var _device = new Object()
         _device.node_id = values.node_id
-        _device.id = message.substring(12)
-        _device.config = message.substring(0, 12)
-        _device.devicetype_id = 1  // 1 = Motion sensor
+        _device.id = values.device_id
+        switch (values.messagetype_id) {
+            case 16:
+                _device.config = message.substring(0, 12)
+                _device.devicetype_id = 1  // 1 = S_MOTION (Motion sensor)
+                break
+            case 2:
+                _device.devicetype_id = 3  // 3 = S_BINARY (Binary device on/off)
+                break
+            case 40:
+                _device.devicetype_id = 26  // 3 = S_RGB_LIGHT (RGB light)
+                break
+        }
         debug('Insert Devices.db: %o', _device)
         db.Devices.create(_device)
             .then(updatedRow => {
